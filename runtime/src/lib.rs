19c7fd38cbfae0eb5e149166685f494deaff1d91
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2022-08-01 10:42:14 +0100

    set minimim validator count in runtime

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -288,6 +288,7 @@ impl pallet_staking_ddpos::Config for Runtime {
 	type Event = Event;
 	type Currency = Balances;
 	type CurrencyBalance = Balance;
+	type MinimumValidatorCount = ConstU32<2>;
 }
 
 // Create the runtime by composing the FRAME pallets that were previously configured.
